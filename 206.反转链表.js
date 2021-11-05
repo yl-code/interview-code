bceedf9367419416f33ccc0e62e4241a2576463e
yl <yl@YL-MacBookPro.local>
2021-11-05 23:39:38 +0800

    init 反转链表

---
+++ 206.反转链表.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let curr = head;
+  let prev = null;
+  let next = null;
+
+  while (curr) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+};
+// @lc code=end
